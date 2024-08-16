1024b809a984f7e6d2cc5bd9662a50f4069e9daa
Vaibhav Shejol <vaibhav.shejol@bnt.local>
2024-08-16 10:26:04 +0530

    API endpoints changed

--- GraphQLExample/src/main/java/com/example/controller/EmployeeController.java
+++ GraphQLExample/src/main/java/com/example/controller/EmployeeController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
+@RequestMapping("/api/employee")
 public class EmployeeController {
 
     private final EmployeeService employeeService;
@@ -17,26 +18,26 @@ public class EmployeeController {
         this.employeeService=employeeService;
     }
 
-    @PostMapping("/employee")
+    @PostMapping
     public Employee addEmployee(@RequestBody Employee employee) {
         return employeeService.createEmployee(employee);
 
     }
 
-//    @GetMapping("/employee/{id}")
+//    @GetMapping("/{id}")
     @QueryMapping("getEmployee")
     public Employee getEmployees(/*@PathVariable("id")*/ @Argument Integer id){
 
         return employeeService.getEmployee(id);
     }
 
-    @GetMapping("/employee")
+    @GetMapping
     public List<Employee> getAllEmployees(){
 
         return employeeService.getAllEmployees();
     }
 
-    @DeleteMapping("/employee/{id}")
+    @DeleteMapping("/{id}")
     public boolean deleteEmployee(@PathVariable("id") Integer id ) {
 
         return employeeService.deleteEmployee(id);
